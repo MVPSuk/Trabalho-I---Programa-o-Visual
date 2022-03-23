b5ca568b02c0c12e1eb31e38468e70fafa1d7e97
MVPSuk <102248521+MVPSuk@users.noreply.github.com>
2022-03-23 14:53:23 -0300

    Add files via upload

---
+++ TrabalhoI_MarcosFelipeNeryMicheletti/Calendario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoI_MarcosFelipeNeryMicheletti
+{
+    public class Calendario
+    {
+        public Byte mes { get; set; }
+        private UInt16 ano { get; set; }
+
+        public Boolean isMesValido()
+        {
+            if (mes > 12 || mes < 1)
+            {
+                return false;
+            }
+            else return true;
+        }
+
+    }
+}
